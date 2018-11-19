52758b3f57606bbdf8fe59393342d02585c08fff
nkambou <43118792+nkambou@users.noreply.github.com>
2018-11-19 07:01:23 -0500

    Add files via upload

---
+++ TP2.pl
@@ -0,0 +1,148 @@
+﻿:- use_module(library(lists)).
+:- ['donneesTp2.pl'].
+
+/* 0) Le predicat listeFilms(L) lie L à la liste (comportant les identifiants) de tous les films. 
+Exemple: 
+        ?- listeFilms(ListeDesFilms).
+        ListeDesFilms = [sica, nuit, coupe, wind, avengers, iron, sherlock, wind2].
+*/
+listeFilms(L) :- findall(X, film(X,_,_,_,_,_,_,_,_), L).
+
+/* 
+1) 0.25pt. Le predicat listeActeurs(L) unifie L à la liste (comportant les identifiants) de tous les acteurs. 
+*/
+
+
+/* 2) 0.25pt. Le predicat experience(IdAct,Annee,Ne) unifie Ne au nombre d'années d'expérience à l'année Annee, de l'acteur dont l"identifiant est IdAct. 
+precondition: Annee doit être définie. 
+*/ 
+
+
+
+/* 
+3) 0.75pt. Le predicat filtreCritere unifie ActId à l'identifiant du premier acteur qui verifie tous les criteres de Lc. 
+precondition: Lc doit etre defini. 
+*/									  
+
+
+/* 
+4) 0.75pt. Le predicat totalSalaireMin(LActeur,Total) calcule la somme des salaires minimuns exigés par les acteurs dont la liste (des identifiants) est spécifiée. 
+*/
+
+
+
+/* 
+5a) 0.75pt. Le prédicat selectionActeursCriteres(Lcriteres,Lacteurs) unifie Lacteurs à la liste formée des identifiants des acteurs qui satisfont tous les critères de Lcriteres.
+precondition: la liste de criteres doit être définie. 
+*/
+
+
+
+/* 
+5b) 1pt. Le prédicat selectionActeursCriteresNouvelle(Lcriteres,Lacteurs,LChoisis) unifie LChoisis à la liste formée des identifiants des acteurs sélectionnés parmi les acteurs dans Lacteurs selon 
+le principe suivant (jusqu'à concurrence de N acteurs, N correspondant au nombre de critères dans LCriteres: le premier acteur qui satisfait le premier critere de Lcriteres, le premier acteur 
+non encore sélectionné et qui satisfait le deuxième critère etc.	
+precondition: la liste de criteres (Lcriteres) et la liste des acteurs contenant leurs idenfiants (Lacteurs) doivent être définies. 
+
+*/
+
+
+
+/* 
+6) 1pt. Le prédicat filmsAdmissibles(ActId,LFilms) unifie LIdFilms à la liste des films (identifiants) satisfaisant les restrictions de l'acteur ActId. 
+*/
+
+
+/* 
+7a) 1pt. Le prédicat selectionActeursFilm(IdFilm,Lacteurs) unifie Lacteurs à la liste formée des identifiants d'acteurs pour lesquels le film de d'identifiant IdFilm satisfait les restrictions.
+préconditions: IdFilm doit être défini 
+*/
+
+
+/*  
+7b) 1pt. Le prédicat selectionNActeursFilm2(IdFilm,Lacteurs) unifie Lacteurs à la liste formée des identifiants des N premiers acteurs pour lesquels le film d'identifiant IdFilm satisfait les restrictions.
+         N étant le nombre d'acteurs du film. Si le nombre total des acteurs qualifiés est inférieur au nombre d'acteurs du film, la liste retournée (Lacteurs) devra contenir l'atome pasAssezDacteur.
+préconditions: IdFilm doit être défini 
+*/
+
+
+/* 
+8) 1pt. Le prédicat acteurJoueDansFilm(Lacteurs, IdFilm) ajoute dans la base de faits tous les acteurs (identifiants) jouant dans le film de titre spécifié (IdFilm) 
+*/
+
+
+/* 
+9a) 1pt. Le prédicat affectationDesRolesSansCriteres(IdFilm) a pour but de distribuer les rôles à une liste d'acteurs pouvant jouer dans le film identifié par IdFilm (puisque
+le film satisfait à ses restrictions). Les N premiers acteurs dont les restructions sont respectées par le film (N correspondant au nombre de rôles du film), sont ajoutés dans
+dans la base de faits par des prédicats "joueDans".
+Ce prédicat modifie le fait film correspondant à IdFilm par destruction et remplacement par un nouveau fait film égal à l'ancien mais dont le budget a été remplacé par la somme des salaires minimums des acteurs choisis et son coût a été diminué de la différence entre le budget initial et le nouveau budget.
+Ce prédicat complète la base de faits joueDans(IdActeur, IdFilm) en fonction des N acteurs sélectionnés et dont la somme des salaires minimums est inférieure ou égale au budget (salarial) du film. Le prédicat doit envisager toutes les combinaisons possibles des N acteurs tirés de la base de faits acteur 
+Le prédicat échoue et ne modifie rien si une des conditions suivantes est vérifiée (dans l'ordre):
+  0) les rôles ont déjà été distribués por ce film
+  1) le réalisateur du film est pasDeRealisateur
+  2) le producteur du film est pasDeProducteur
+  3) s'il n'y a pas assez d'acteurs,
+  4) si le budget du film est insuffisant.
+précondition: L'identifiant du film doit être défini.
+*/
+
+
+/*
+9b) 1pt. Le prédicat affectationDesRolesCriteres(IdFilm,Lcriteres,LChoisis) unifie LChoisis à la liste d'acteurs satisfaisant aux critères de sélection du film, Ce film doit bien entendu satisfaire aux restrictions de 
+chacun des acteurs candidat. 
+Dans ce prédicat, IdFilm est un identifiant de film et Lcriteres est une liste de critères. 
+Pour la satisfaction des critère, on retiendra toujours le premier acteur satisfaisant au 1er critère et on recommensera avec le même principe pour les autres acteurs et les critères restants.
+Contrairement au prédicat affectationDesRolesSansCriteres défini à la question 9a, affectationDesRolesCriteres ne modifie pas ba base de faits et se contente de récupérer la liste des acteurs sélectionnés dans Lchoisis.
+Le prédicat échoue
+  1) si la liste des critère est vide,
+  2) si le réalisateur du film est pasDeRealisateur,
+  3) si le producteur du film est pasDeProducteur,
+  4) s'il n'y a pas assez d'acteurs.
+précondition: L'identifiant du film et la liste de critères doivent être définis.
+Attention: Il est possible qu'il y ait moins de critère que d'acteurs admissibles. Dans ce cas, la liste des acteurs sélectionnés ne peut dépasser le nombre de critères dans Lcriteres.
+           Le nombre maximum d'acteurs choisis est donc égal à la taille de la liste Lcriteres.
+*/
+
+
+
+/*
+10) 2pts. Le prédicat affectationDesRoles(IdFilm, Lcriteres) a pour but de distribuer les rôles à une liste d'acteurs pouvant jouer dans le film et satisfaisant
+aux critères de sélection du film en ajoutant les acteurs choisis dans la base de faits "joueDans".
+Dans ce prédicat, IdFilm est un identifiant de film et Lcriteres est une liste de critères. 
+Pour la satisfaction des critère, on retiendra toujours le premier acteur satisfaisant au 1er critère et on recommensera avec le même principe pour les autres acteurs et les critères restants.
+Ce prédicat modifie le fait film correspondant à IdFilm par destruction et remplacement par un nouveau fait film égal à l'ancien mais dont le budget a été remplacé par la somme des salaires minimums des acteurs choisis et son coût a été diminué de la différence entre le budget initial et le nouveau budget.
+Ce prédicat complète la base de faits joueDans(IdActeurActeur, IdFilm) en fonction des n acteurs sélectionnés (où n est le nombre de rôles du film) qui satisfont tous les critères de Lcriteres, pour lesquels le film satisfait leur restrictions et dont la somme des salaires minimums est inférieure ou égale au budget du film. 
+Le prédicat doit envisager toutes les combinaisons possibles des n acteurs tirés de la base de faits acteur.
+Le prédicat échoue et ne modifie rien 
+  1) si le réalisateur du film est pasDeRealisateur,
+  2) si le producteur du film est pasDeProducteur,
+  3) s'il n'y a pas assez d'acteurs,
+  4) si le budget du film est insuffisant pour financer le salaire minimum de tous acteurs sélectionnés.
+précondition: L'identifiant du film et la liste de critères doivent être définis.
+Attention: 
+1) Il est possible qu'il y ait moins de critère que d'acteurs admissibles. Dans ce cas, la liste des acteurs sélectionnés doit être complétée (si possible et à concurrence de nombre de rôles) selon le principe du prédicat affectationDesRolesSansCriteres(IdFilm) de la question 9a.
+2) Si la liste Lcriteres est vide, c'est aussi le principe de affectationDesRolesSansCriteres(IdFilm) de la question 9a qui s'applique.
+*/
+
+
+
+/* 11) 1,25 pts. Le prédicat produire(NomMaison,IdFilm) vérifie si la maison peut produire le film identifié. Il vérifie si le budget de la maison 
+est supérieur au cout du film, si le réalisateur n'est pas pasDeRealisateur, et si le producteur est pasDeProducteur. Si la production est possible,
+ on diminue le budget de la maison par le coût du film et on remplace le fait 'film' par un nouveau film égal à l'ancien sauf que la composante producteur 
+ est égale à NomMaison. 
+ Précondition: le nom de la maison et l'identifiant du film doivent être connus. Le prédicat doit échoué si la maison ne peut pas produire le film. 
+*/
+
+ 
+							 
+/* 12) 0.75pt. Le prédicat plusieursFilms(N,Lacteurs) unifie Acteurs à la liste des acteurs (comportant leurs NOMS), qui jouent dans au moins N films.
+N doit être lié à une valeur au moment de la requête de résolution du but 
+*/
+
+
+
+/* 13) 1.25pt. Les films réalisés et produits doivent maintenant être distribués dans les cinémas. On vous demande définir le prédicat distribuerFilm(IdFilm,PrixEntree) qui envoie le film identifié par IdFilm à tous les cinémas en spécifiant le prix d'entrée suggéré. 
+Ce prédicat doit modifier la base de connaissances en ajoutant le triplet  (IdFilm,0,PrixEntree) dans le répertoire de chacun des cinémas déjà existants.
+ */
+
+					
---
+++ donneesTP2.pl
@@ -0,0 +1,87 @@
+﻿% joueDans est un prédicat dynamique dont les clauses sont créées à mesure que les rôles sont attribués aux acteurs.
+% format: joueDanns(identifianfActeur, identifiantFilm)
+% Exemple: joueDans(merryl, wind2).
+:- dynamic joueDans/2. %prédicat dynamique d'ariété 2 
+
+% les réalisateurs
+% format: realisateur(Identifiant, Nom, Adresse)
+realisateur(woody, 'Allen Woody', 'New York, New York').
+realisateur(arcan, 'Arcan Denis', 'Montréal, Québec').
+realisateur(villeneuve, 'Denis Villeneuve', 'Montréal, Québec').
+realisateur(silver, 'Guy Silver', 'Boston, MA').
+realisateur(joss, 'Joss Marvel', 'Hollywood, CA').
+realisateur(jon, 'Jon Marvel', 'Los Angeles, CA').
+
+% les acteurs	
+% format: acteur(Nom,Sexe,RevenuMin,DateDebut,Restriction).						 
+acteur('Schawrznnegger Arnold',ma,1000,date(1978,4,1),arnold).
+acteur('Streep Meryll',fe,1500,date(1984,9,1),merryl).
+acteur('Farfelu Maximo',ma,1, date(2015,1,1),maxi).
+acteur('Roberts Julia',fe,2000,date(1985,4,12),julia).
+acteur('Reno Jean',ma,100, date(1975,8,15),reno).
+acteur('Stone Sharon',fe,500,date(1990,8,15),sharon).
+acteur('Downey Jr Robert',ma,4500,date(1997,10,1),robert).
+
+% les restrictions des acteurs
+% format: identifiantActeurConcern(predicatFilm).
+arnold(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,_,Realisateur,Producteur,Cout,_,_,_),
+                         \+ Realisateur = pasDeRealisateur,
+                         \+ Producteur = pasDeProducteur, Cout > 100000.
+merryl(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,Type,Realisateur,_,_,Duree,NombreActeurs,_),
+                         \+ Type = action, \+ Realisateur = pasDeRealisateur,
+                         (Duree > 90; NombreActeurs = 2).
+maxi(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,_,Realisateur,_,_,_,_,_),
+                                 realisateur(Realisateur,_,Adr), (Realisateur = woody; sub_string(Adr, _, _, _, 'Québec')).
+julia(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,Type,_,_,_,_,_,Budget), (Type = drame; Type = humour),Budget < 4000.
+reno(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,drame,_,_,_,_,_,_).
+sharon(film(F,_,_,_,_,_,_,_,_)) :- film(F,Titre,_,Realisateur,_,_,_,_,_),
+                             name(Titre,Tl),length(Tl,Lt), Lt> 4, Lt<15, \+ Realisateur = woody.
+robert(film(F,_,_,_,_,_,_,_,_)) :- film(F,_,action,_,_,_,_,_,_).
+
+
+% les producteurs
+% format: maison(Nom, Budget)
+% dymanique car le budget changera après une décision de production.
+:- dynamic maison/2.
+maison('Flash Films', 100000).
+maison('United Artists',1000000).
+maison('Flop Films', 50).
+maison('Black Label Media',1000000).
+maison('Fox',1000000).
+maison('Vivendi',1000000).
+
+% les films
+% format: film(IdFilm,TitreFilm,TypeFilm,RealisateurFilm,ProducteurFilm,CoutFilm,DureeFilm,NbaFilm,BudgetFilm)
+% dymanique car plusieurs aspects du film peuvent changés. Exemples: RealisateurFilm, ProducteurFilm, BudgetFilm, CoutFilm
+:- dynamic film/9.
+film(sica, 'Sicaire', action, villeneuve, 'Black Label Media', 100000, 95, 150, 60000).
+film(nuit, 'Une nuit a Casablanca', humour, woody, pasDeProducteur,2000,100,3,1000).
+film(coupe, 'Coup de coupe !', horreur, arcan,'Flop Films', 45000,45,1,10000).
+film(wind, 'Gone with the wind !', drame, woody, 'Flash Films',450,120,4,100).
+film(wind2, 'Gone with the wind, the sequell !','drame',woody,'Flash Films',4500,120,2,3999). 
+film(avengers, 'The Avengers', action, joss,'Fox', 200000, 143, 2, 110000 ).
+film(iron, 'Iron Man', action, jon, 'Vivendi', 16000, 126, 3, 100000).
+film(sherlock, 'Sherlock Holmes', policier, silver, 'Vivendi', 90000, 128, 1, 90000 ).
+	
+/* les cinemas
+format: cinema/3(identifiant, addresse, repertoireDesFilms)
+        repertoireDesFilms est une liste de triplets de forme (identifiantFilm, NbreEntrees, PrixEntree).
+		Exemple: cinema(cineparc, 'Montreal Quebec', [(sherlock, 25500, 8), (iron, 36300, 11), (avengers, 40020, 10)]).
+*/
+:- dynamic cinema/3.
+cinema(starcite, 'Montreal Quebec', []).
+cinema(cineparc, 'Montreal Quebec', []).
+cinema(theatreoutremont, 'laval', []).
+cinema(centrephi, 'Longueuil', []).
+
+/* les critères de rôle pour un acteur...
+format: critere/2(IdentifiantDuCritere, But) suivi d'une définion, soit la condition dans lesquelles But est prouvé.
+        Exemple de but: acteur(_,Sexe,_,_,IdAct)
+		Exemple de critère: critere(masculin,(acteur(_,Sexe,_,_,IdAct))) :- Sexe = ma, acteur(_,Sexe,_,_,IdAct).
+*/
+critere(masculin,(acteur(_,Sexe,_,_,IdAct))) :- Sexe = ma, acteur(_,Sexe,_,_,IdAct).
+critere(feminin,(acteur(_,Sexe,_,_,IdAct))):- Sexe = fe, acteur(_,Sexe,_,_,IdAct).
+critere(experienceMin,(acteur(NA,_,_,_,IdAct))) :- acteur(NA,_,_,date(Adebut,_,_),IdAct), (2016-Adebut) > 5. %acteurs ayant au moins 5 ans d'expérience au 1er janvier 2015
+critere(experienceMax,(acteur(NA,_,_,_,IdAct))) :- acteur(NA,_,_,date(Adebut,_,_),IdAct), (2016-Adebut) =< 25. %acteurs avec au plus 25 ans d'expérience au 1er janvier 2015
+critere(revenuMin,(acteur(NA,_,R,_,IdAct))) :- acteur(NA,_,R,_,IdAct), R > 1000. % acteurs avec au moins 1000$ pour revenuMin 
+critere(revenuMax,(acteur(NA,_,R,_,IdAct))) :- acteur(NA,_,R,_,IdAct), R >= 10000. %acteurs avec au moins 10000$ pour revenuMin
